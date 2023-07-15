a19bea77a9f8858d8ab930c134740be48f26bc35
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-15 21:40:51 +0545

    chakaton

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-let container = document.getElementById("container");
+let API = "http://localhost:8000/students";
+let student = document.getElementById("student");
 let list = document.getElementById("list");
 let btnSend = document.getElementById("btn-send");
 let surname = document.getElementById("surname");
 
-container.addEventListener("click", function () {
-  container.style.display = "none";
-  list.style.display = "block";
-});
+// student.addEventListener("click", function () {
+//   student.style.display = "none";
+//   list.style.display = "block";
+// });
 
-list.addEventListener("click", function () {
-  list.style.display = "block";
-  container.style.display = "none";
-});
+// list.addEventListener("click", function () {
+//   list.style.display = "block";
+//   student.style.display = "none";
+// });
 btnSend.addEventListener("click", send);
-function send() {}
+async function send() {
+  let newClick = {
+    students: btnSend.value,
+  };
+  if (newClick.students.trim() === "") {
+    alert("Заполните поле !");
+    return;
+  }
+  await fetch(API, {
+    body: JSON.stringify(newClick),
+  });
+}
 surname.addEventListener("click", function () {});
